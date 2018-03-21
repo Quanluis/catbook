2cf42df28f964cd5367e56074b557517e15b60e0
quan <quanluis@protonmail.com>
2018-03-20 20:36:09 -0400

    Created the Basic Welcome Layout for the Catbook, Adding the logic to redirect the individual to the profile maker once the user clicks the create profile button

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+        <div>
+          <h1>Catbook</h1>
+          <hr width = '70%' />
+        </div>
+            <div>
+             <p>
+             Welcome to the Catbook! Create a profile for your furry friend and have the Option for 
+             others to see them!  
+             </p>
+            </div>
+        <div>
+        <button >Create Profile</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
