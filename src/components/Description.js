562c073683f9628ab461afdc5cd72a382730b0cc
quan <quanluis@protonmail.com>
2018-03-23 19:06:13 -0400

    Changed input field to textarea for better user input on description component.

--- src/components/Description.js
+++ src/components/Description.js
@@ -10,8 +10,11 @@ class Description extends React.Component{
                 <hr width = "40%"/> 
                 <p>Show off some Cool info,story,Anything about your friend!</p>
                 <br/>   
-                <form action="description">
-                <input className = 'description' placeholder = "Description" type="description-text"rows="10"/>
+                <form className="description">
+               
+                <textarea className = 'des-box' rows = '5' col= '40'>
+                    </textarea>
+                
                 </form>
             </div>
         )
