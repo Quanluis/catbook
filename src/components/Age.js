3016a3f5e6d4564be4b3ff324038105171e1628e
quan <quanluis@protonmail.com>
2018-03-23 18:31:47 -0400

    Created input value for cats Age to be set from 0-25 human years added function to make sure input value responds accordingly .

--- src/components/Age.js
+++ src/components/Age.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
+
+
+
+
 class Age extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            age: ''
+        }
+        this.inputAge = this.inputAge.bind(this)
+    }
+    inputAge(e){
+        this.setState({
+            age: +e.target.value
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>
-                    Age
+                    Age (Human Years)
                 </h1>
                 <hr width = "20%"/>
                 <br/>
                 <form action="">
-                <input className = "age" type="number"/>
+                <input 
+                 min = "1"
+                 max = '25'
+                 onChange = {(e) => this.inputAge(e)} value = {this.state.age} className = "age" type="number"/>
                 </form>
             </div>
         )
