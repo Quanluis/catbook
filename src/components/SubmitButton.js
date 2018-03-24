399e781207b0e289495f647e53ed6b3401a57dd6
quan <quanluis@protonmail.com>
2018-03-24 16:32:06 -0400

    Created a onclick function that registers the amount of clicks.

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class SubmitButton extends React.Component{
+
+    constructor(){
+        super()
+        this.State = {
+            
+
+        }
+    }
+    
+
     render(){
         return(
             <div>
-                <button>Submit</button>
+                <button onClick = { (e) => console.log('hello')} >Submit</button>
             </div>
         )
     }
