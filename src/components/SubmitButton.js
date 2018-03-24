4cad00536eb3b60fba605b0f0cecce5250ed5907
quan <quanluis@protonmail.com>
2018-03-24 13:49:27 -0400

    Created a simple submit button component

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SubmitButton extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
+export default SubmitButton;
