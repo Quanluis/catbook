84ac92e4a37c50df3bb8624190b34e2656681104
quan <quanluis@protonmail.com>
2018-03-26 22:41:13 -0400

    Made sure the basic signup component was finished and linked it to the react router within the index.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,23 @@
+import React from 'react'; 
+
+class SignUp extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    Sign Up 
+                </h1>
+                <hr width = '40%'/>
+                <p> Sign up with a valid email adress and password </p>
+                <form>
+                    <input className ="form" type="email" placeholder = "email"/>
+                    <br/>
+                    <input className = "form" type="password" placeholder = "Enter password" />
+                    <br/>
+                    <input className = "form" type="password" placeHolder = "Enter Password twice"/>
+                </form>
+            </div>
+        )
+    }
+}
+export default SignUp; 
