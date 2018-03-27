b0560a93612e1c18909a7f54397cc83bc5f32a09
quan <quanluis@protonmail.com>
2018-03-27 00:08:04 -0400

    Created the basic skeleton for the main component that will allow you to sign in or create a account.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class Main extends React.Component{
     render(){
         return(
             <div>
-                <button className = "login-signup">Sign Up</button>
-                <button className = "login-signup"> Login</button>  
+                <h1>Login</h1>
+                <hr width = '30%'/>
+                <form>
+                    <br/> 
+                <input className ="form" type="email" placeholder = "Email"/>
+                    <br/>
+                 <input className = "form" type="password" placeholder = "Enter Password" />
+                <button>Login</button>
+                </form>
+                <Link className = "link" to = './Signup'> signup </Link>
             </div>
         )
     }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,13 +8,14 @@ class SignUp extends React.Component {
                     Sign Up 
                 </h1>
                 <hr width = '40%'/>
-                <p> Sign up with a valid email adress and password </p>
+                <p> Sign up with a valid email address and password. </p>
                 <form>
                     <input className ="form" type="email" placeholder = "email"/>
                     <br/>
                     <input className = "form" type="password" placeholder = "Enter password" />
                     <br/>
                     <input className = "form" type="password" placeHolder = "Enter Password twice"/>
+                    <button>Submit</button>
                 </form>
             </div>
         )
