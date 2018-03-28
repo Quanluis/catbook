ccfb7d68ca1f3619da8b01a5dc3a8a784503fc24
quan <quanluis@protonmail.com>
2018-03-28 19:02:54 -0400

    Same commit as before.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-class Main extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            email: "",
-            password: "",
-            isloggedIn: false
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      isloggedIn: false
+    };
+  }
+  userEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
 
-        };
-    }
-    userEmail(event){
-        this.setState({
-            email: event.target.value
-        })
-    }
+  userPassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
 
-
-    userPassword(event){
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-
-    render(){
-        return(
-            <div>
-                <h1>Login</h1>
-                <hr width = '30%'/>
-                <form action = "submit" >
-                    <br/> 
-                <input value = {this.state.email}
-                onChange = {this.userEmail.bind(this)}
-                type = 'email'  
-                className ="form"  placeholder = "email"/>
-                    <br/>
-                 <input value = {this.state.password}
-                   onChange = {this.userPassword.bind(this)}
-                  className = "form" type="password" placeholder = "Enter Password" />
-                <button>Login</button>     
-                </form>          
-                <Link className = "link" to = './createProfile'> Login </Link> 
-                <Link className = "link" to = './Signup'> signup </Link>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <hr width="30%" />
+        <form action="submit">
+          <br />
+          <input
+            value={this.state.email}
+            onChange={this.userEmail.bind(this)}
+            type="email"
+            className="form"
+            placeholder="email"
+          />
+          <br />
+          <input
+            value={this.state.password}
+            onChange={this.userPassword.bind(this)}
+            className="form"
+            type="password"
+            placeholder="Enter Password"
+          />
+          <button>Login</button>
+        </form>
+        <Link className="link" to="./createProfile">
+          {" "}
+          Login{" "}
+        </Link>
+        <Link className="link" to="./Signup">
+          {" "}
+          signup{" "}
+        </Link>
+      </div>
+    );
+  }
 }
 export default Main;
