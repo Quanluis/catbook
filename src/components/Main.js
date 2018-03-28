1af4025398185cd9bce135fb0bd327433bf16a13
quan <quanluis@protonmail.com>
2018-03-28 18:49:20 -0400

    Added logic for the main component that allow the user to add email and password inputs and are stored in the state.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 class Main extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            email: "",
+            password: ""
+
+        };
+    }
+    userEmail(event){
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+
+    userPassword(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+
     render(){
         return(
             <div>
                 <h1>Login</h1>
                 <hr width = '30%'/>
-                <form>
+                <form action = "submit" >
                     <br/> 
-                <input className ="form" type="email" placeholder = "Email"/>
+                <input value = {this.state.email}
+                onChange = {this.userEmail.bind(this)}
+                type = 'email'  
+                className ="form"  placeholder = "email"/>
                     <br/>
-                 <input className = "form" type="password" placeholder = "Enter Password" />
+                 <input value = {this.state.password}
+                   onChange = {this.userPassword.bind(this)}
+                  className = "form" type="password" placeholder = "Enter Password" />
                 <button>Login</button>     
                 </form>          
                 <Link className = "link" to = './createProfile'> Login </Link> 
